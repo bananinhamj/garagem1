107d471c47a116350c4e407e349c4416dd150320
bananinhamj <viliczinskimj@gmail.com>
2023-03-14 16:23:01 -0300

    Att modelo marca modelo categoria criada

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from garagem.models import Marca, Categoria 
+from garagem.models import Categoria, Marca
 
-admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Marca)
