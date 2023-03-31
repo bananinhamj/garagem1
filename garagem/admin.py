4e8461b1c925bd388c5bc69ea9801ebf3086d1bf
bananinhamj <viliczinskimj@gmail.com>
2023-03-31 16:03:28 -0300

    model.veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from garagem.models import Acessorio, Categoria, Cor, Marca
+from garagem.models import Acessorio, Categoria, Cor, Marca, Veiculo
 
 admin.site.register(Acessorio)
 admin.site.register(Categoria)
 admin.site.register(Cor)
 admin.site.register(Marca)
+admin.site.register(Veiculo)
--- garagem/models.py
+++ garagem/models.py
@@ -29,3 +29,14 @@ class Cor(models.Model):
         
     class Meta:
         verbose_name_plural = 'Cores'
+
+class Veiculo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name ="veiculo")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name ="categoria")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name ="cor")
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)    
+    
+    def __str__(self):
+        return f"{self.marca}   {self.categoria}   {self.ano}  {self.cor}"
+    
