90dc38ece1b24c8e22de3c1d557f214570e4efb3
bananinhamj <viliczinskimj@gmail.com>
2023-03-31 16:27:50 -0300

    Atualização Garagem

--- garagem/models.py
+++ garagem/models.py
@@ -20,7 +20,8 @@ class Acessorio(models.Model):
 
     def __str__(self):
         return self.descricao
-
+    class Meta:
+        verbose_name_plural = 'Acessórios'
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
 
@@ -31,12 +32,13 @@ class Cor(models.Model):
         verbose_name_plural = 'Cores'
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name ="veiculo")
-    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name ="categoria")
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name ="cor")
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name ="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name ="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name ="veiculos")
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)    
     
     def __str__(self):
-        return f"{self.marca}   {self.categoria}   {self.ano}  {self.cor}"
-    
+        return f"{self.marca} {self.categoria} {self.ano} {self.cor}"
+    class Meta:
+        verbose_name_plural = 'Veículos'
